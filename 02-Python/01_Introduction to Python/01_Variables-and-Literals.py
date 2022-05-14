e68a11765908cbfaa80dbc385e8d2bc9ed18d2dc
unknown <binoootuliao@gmail.com>
2022-05-14 21:53:38 +0800

    Updated 01_Variables-and-Literals.py

--- 02-Python/01_Introduction to Python/01_Variables-and-Literals.py
+++ 02-Python/01_Introduction to Python/01_Variables-and-Literals.py
@@ -1,9 +1,44 @@
+
+# PART 1
 # Variables = A Container of Values (Data).
 # In this example; both (a) and (b) are variables who contains a typed-value of its own.
 
 
 a = 5
 b = 100
+print(a,b)
 
 # a is a variable that contains a value of 5.
 # b is also a variable that contains a value of 100.
+
+#----------------------------------------#
+
+# PART 2
+# Variables also respects on how you order your variables.
+# In this case, you will see that our variable "c" will only use the latest typed down value.
+
+c = 200
+c = 100
+print(c)
+
+#----------------------------------------#
+
+# PART 3
+# In Variables, we can use letters from (a-z), numbers from (0-9), or an underscore (_).
+# In this example we will use a word as a variable to contain a fixed value called literals.
+
+Android_name = 101021
+print(Android_name)
+
+# The word "Android_name" has a literal value of "101021", which means if we print out "Android_name" it will give us "101021"
+# We can also use words variable to represent another word value of its own.
+# However, In the case of letter values or a composition of both letter and numerical values, it is required to put it inside of a (" ") or (' ') for example:
+
+first_name = "Bino"
+print(first_name)
+
+last_name = "Bino123"
+print(last_name)
+
+
+

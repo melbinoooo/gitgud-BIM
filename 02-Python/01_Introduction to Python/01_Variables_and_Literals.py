2221ac029793f3da6443ac44a4cb42043eddc399
unknown <binoootuliao@gmail.com>
2022-05-14 21:10:45 +0800

    added 01_Variables_)and_Literals

---
+++ 02-Python/01_Introduction to Python/01_Variables_and_Literals.py
@@ -0,0 +1,9 @@
+# Variables = A Container of Values (Data).
+# In this example; both (a) and (b) are variables who contains a typed-value of its own.
+
+
+a = 5
+b = 100
+
+# a is a variable that contains a value of 5.
+# b is also a variable that contains a value of 100.

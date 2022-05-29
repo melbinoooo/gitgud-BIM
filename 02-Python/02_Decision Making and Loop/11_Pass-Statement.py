c9797c84a72757271958d93705fcca1fa37aea07
unknown <binoootuliao@gmail.com>
2022-05-29 18:29:48 +0800

    Updated 11_Pass-Statement.py

--- 02-Python/02_Decision Making and Loop/11_Pass-Statement.py
+++ 02-Python/02_Decision Making and Loop/11_Pass-Statement.py
@@ -1,5 +1,13 @@
 # What is Pass Statement?
+# Pass statement is used to reserved a code line to be able to used in the future.
+
 
 
 #----------------------------------------#
 # Part 1:
+
+sample_sequence = {'a', 'b', 'c', 'd', 'e'}
+
+for val in sample_sequence:
+    pass #Do nothing for now
+print('this is print statement after loop')

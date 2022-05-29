c16e1011a100b80d0984f5068887f5ddad9b0fa1
unknown <binoootuliao@gmail.com>
2022-05-29 20:05:05 +0800

    Updated 14_Anonymous-Function.py

--- 02-Python/03_Functions/14_Anonymous-Function.py
+++ 02-Python/03_Functions/14_Anonymous-Function.py
@@ -1,7 +1,31 @@
-# Function and Arguments Deepdive
+# Anonymous Functions
 
+# We can define a function without defining its name.
 
+# The syntax for the Lambda function is
+# function = lambda expression
 
 
 #----------------------------------------#
-# Part 1:
+# Part 1: Lambda Function
+
+square = lambda n: n*n # This is a lambda function
+
+result = square(3) # This is a Function Call
+
+print(result) # This is the print statement
+#Output : 9
+
+
+
+
+
+# The above program is equivalent to:
+
+
+def n_square(x): # This is a defined function
+    return x*x # This is a return statement
+
+result = n_square(3) # This is a Function Call
+print(result) # This is the print statement
+#Output : 9

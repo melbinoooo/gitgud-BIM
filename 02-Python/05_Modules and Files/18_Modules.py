08bd98d8b97526ff515f16198165229f92ba4721
unknown <binoootuliao@gmail.com>
2022-06-09 05:35:37 +0800

    Updated 18_Modules.py

--- 02-Python/05_Modules and Files/18_Modules.py
+++ 02-Python/05_Modules and Files/18_Modules.py
@@ -6,10 +6,51 @@
 
 #----------------------------------------#
 # Part 1: Creating and Importing Modules
-# We are using 19_Sample-Module.py
+# We are using SampleModule.py
 
 
 
 #----------------------------------------#
-# Part 2: Importing 19_Sample-Module.py
+# Part 2: Importing SampleModule.py
+# And now we can use the functions inside SampleModule by using it as an extention.
+
+
+import SampleModule as SM
+
+result = SM.add(2,4)
+print(result) # Output: 6
+
+
+
+#----------------------------------------#
+# Part 3: Importing Built in Modules in Python like Math
+
+import math
+
+x = 5
+y = 10
+
+
+# Get SquareRoot
+get_square_root = math.sqrt(x)
+print(get_square_root) # Output: 2.23606797749
+
+# Get Log of Y
+get_log = math.log(y)
+print("Log of Y: ", get_log) # Output: Log of Y:  2.302585092994046
+
+
+#----------------------------------------#
+# Part 4: Importing Selected Functions only.
+# Using "From" then "Import"
+
+from math import pi, e
+# We Import only the pi & e
+
+pi_times_x = x * pi
+print(pi_times_x) # Output: 15.707963267948966
+
+# Print 'e' value from math.
+print("Print e-value: ",e) # Output: Print e-value:  2.718281828459045
+
 

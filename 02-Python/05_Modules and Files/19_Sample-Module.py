6fa48b64f508caaa18783ac33a52ee03834be36d
unknown <binoootuliao@gmail.com>
2022-06-09 05:13:04 +0800

    Added 18_Sample-Module.py

---
+++ 02-Python/05_Modules and Files/19_Sample-Module.py
@@ -0,0 +1,9 @@
+# This is a sample Module
+
+#----------------------------------------#
+# Part 1: Creating the module
+
+def add(a,b):
+    """Comment: This Program Add Numbers"""
+    result = a + b
+    return result

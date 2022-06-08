88d5fe08784e396d38e424b7a25ac931343384a5
unknown <binoootuliao@gmail.com>
2022-06-09 05:10:38 +0800

    Added 18_Modules.py

--- 02-Python/05_Modules and Files/18_Modules.py
+++ 02-Python/05_Modules and Files/18_Modules.py
@@ -1,8 +1,9 @@
-#
+# What are modules? 
+# Modules refers to a container which contain statement and definitions in Python.
+# Modules are used to break down large chunk of codes into small ones.
+
 
 
 #----------------------------------------#
-
-
-
-# Part 1:
+# Part 1: Creating and Importing Modules
+# We are using 19_Sample-Modle.py
